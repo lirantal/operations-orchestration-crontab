5c9226ccd58f3f207844e2795a3d9c9f4a66b063
Liran Tal <liran.tal@gmail.com>
2015-12-19 14:46:35 +0200

    initial project commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# operations-orchestration-crontab
+NodeJS Tool for scheduling and converting crontab entries to HPE's Operations Orchestration
+
+# About
+`operations-orchestration-crontab` is a handy console command line utility which allows to easily read and parse a linux crontab file and created scheduled flows to a remote installed deployment of HPE's Operations Orchestration application in order to quickly convert existing crontab entries to OO product.
+
+# Install
+Install the tool easily with npm, after which the tool will be available in your command line prompt to run.
+
+```javascript
+npm install operations-orchestration-crontab
+```
+
+# Usage
+
+
+
+# Author
+Liran Tal <liran.tal@gmail.com>
---
+++ index.js
@@ -0,0 +1,192 @@
+'use strict'
+/**
+ * operations-orchestration-crontab
+ * @module operations-orchestration-crontab
+ */
+
+var OO 				= require('operations-orchestration-api');
+var commandLineArgs = require('command-line-args');
+var jsonfile 		= require('jsonfile');
+var chalk			= require('chalk');
+
+var options = {
+	username: 'admin',
+	password: 'admin',
+	baseUrl: 'http://localhost:8050'
+};
+
+/**
+ * get module package information
+ * 
+ * @method	getPackageInfo
+ * @return	{string}		package information as a sring
+ */
+function getPackageInfo() {
+
+	var pkg = require('./package.json');
+
+	var str = '';
+	str += pkg.name + "\n";
+	str += 'version: ' + pkg.version + ' by ' + pkg.author + "\n";
+	return str;
+}
+
+/**
+ * prints out command line usage information and exit with an error code
+ * 
+ * @method	cliShowUsage
+ * @param 	{string}	command line arguments usage help
+ * @param 	{string}	the error message to print to the screen
+ */
+function cliShowUsage(cliUsage, msg) {
+
+	console.log(getPackageInfo());
+	console.log(chalk.red(Error(msg)));
+	console.error(cliUsage);
+	process.exit(1);
+}
+
+/**
+ * prints out an error message
+ * 
+ * @method	cliExitError
+ * @param 	{string}	the error message to print on the screen
+ */
+function cliExitError(msg) {
+
+	console.log(getPackageInfo());
+	console.log(chalk.red(msg));
+	process.exit(1);
+}
+
+/**
+ * prints out a success message
+ * 
+ * @method	cliExitClean
+ * @param 	{string}	a success message text to print to the screen
+ */
+function cliExitClean(msg) {
+
+	console.log(chalk.green(msg));
+	process.exit(0);
+}
+
+/**
+ * parse command line arguments to set the options for connecting to OO 
+ * 
+ * @method	cliCheck
+ * @return	{object}		return the object with the passed parameters to the command line
+ */
+function cliCheck() {
+
+	var cli = commandLineArgs([
+	  { name: 'username', alias: 'u', type: String, description: 'Username for Operations Orchestration that is allowed to query the API' },
+	  { name: 'password', alias: 'p', type: String, description: 'Password for the Username provided' },
+	  { name: 'url', type: String, description: 'The URL where Operations Orchestration API is available. Example: http://localhost:8050' },
+	  { name: 'crontab', type: String, description: 'Crontab file name to read and parse for creating OO scheduled flows' },
+	]);
+
+	var cliOptions = cli.parse();
+	var cliUsage = cli.getUsage();
+
+	if (!cliOptions.url) {
+		cliShowUsage(cliUsage, "must provide url for the OO REST API server");
+		return false;
+	} else {
+		options.baseUrl = cliOptions.url + '/oo/rest/v1';
+	}
+
+	if (cliOptions.username) {
+		options.username = cliOptions.username;
+	}
+
+	if (cliOptions.password) {
+		options.password = cliOptions.password;
+	}
+
+	if (!cliOptions.crontab) {
+		cliShowUsage(cliUsage, "must provide option of --crontab for a crontab file entry to read");
+		return false;
+	}
+
+	// set OO API settings
+	OO.setClient(options);
+	return cliOptions;
+}
+
+/**
+ * imports configuration from a JSON file into an OO install
+ * 
+ * @method	importConfig
+ * @param 	{object}	the command line arguments options object
+ */
+function importConfig(options) {
+
+	jsonfile.readFile(options.import, function(err, obj) {
+
+		if (err) {
+			cliExitError(err);
+		}
+
+		var item = '';
+		for (var configItem of obj) {
+
+			item = {
+				type: configItem.type,
+				path: configItem.path,
+				value: configItem.value
+			};
+
+			OO.config.setItem(item, function(err, body) {
+				if (err) {
+					process.stdout.write(chalk.red('-'));
+				} else {
+					process.stdout.write(chalk.green('+'));
+				}
+			});
+
+		}
+		
+	});
+
+}
+
+/**
+ * exports an OO install configuration to a JSON file
+ * 
+ * @method	exportConfig
+ * @param 	{object}	the command line arguments options object
+ */
+function exportConfig(options) {
+
+	if (!options.export) {
+		cliExitError(Error("must provide export file name"));
+		return false;
+	}
+
+	OO.config.getAllItems(function(err, body) {
+
+		if (err) {
+			cliExitError(err);
+		}
+
+		if (body) {
+			jsonfile.writeFile(options.export, body, {spaces: 2}, function(err) {
+				if (err) {
+					cliExitError(err);
+				}
+
+				cliExitClean("Successfully export OO configuration to: " + options.export);
+				return true;
+			});
+		}
+	});
+}
+
+
+var cliOptions = cliCheck();
+if (!cliOptions) {
+	cliExitError();
+}
+
+console.log(getPackageInfo());
